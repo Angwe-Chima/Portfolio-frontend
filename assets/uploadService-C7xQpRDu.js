74ec8b4a076fd0648d452ccbdd0d8be8b4bb4df3
Angwe Chima <angwechima@gmail.com>
2026-01-02 21:00:40 +0100

    Updates

---
+++ assets/uploadService-C7xQpRDu.js
@@ -0,0 +1,1 @@
+import{J as e}from"./index-BL_n9eQa.js";const r=async t=>{const a=new FormData;return a.append("image",t),(await e.post("/upload/image",a,{headers:{"Content-Type":"multipart/form-data"}})).data};export{r as u};
